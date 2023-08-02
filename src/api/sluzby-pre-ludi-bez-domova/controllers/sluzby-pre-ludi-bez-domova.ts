346c6bf688909acff16580d67b04d08f1fd89558
Jakub Hruby <jakub.hruby@bratislava.sk>
2023-08-02 10:22:53 +0200

    Layer sluzby pre ludi bez domova added

---
+++ src/api/sluzby-pre-ludi-bez-domova/controllers/sluzby-pre-ludi-bez-domova.ts
@@ -0,0 +1,7 @@
+/**
+ * sluzby-pre-ludi-bez-domova controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::sluzby-pre-ludi-bez-domova.sluzby-pre-ludi-bez-domova');
---
+++ src/api/sluzby-pre-ludi-bez-domova/services/sluzby-pre-ludi-bez-domova.ts
@@ -0,0 +1,7 @@
+/**
+ * sluzby-pre-ludi-bez-domova service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::sluzby-pre-ludi-bez-domova.sluzby-pre-ludi-bez-domova');
