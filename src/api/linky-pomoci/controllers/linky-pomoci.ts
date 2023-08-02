37b13fde4a0c172bba0ac55ee787146dda141a64
Jakub Hruby <jakub.hruby@bratislava.sk>
2023-08-02 10:08:15 +0200

    Added linky pomoci layer

---
+++ src/api/linky-pomoci/controllers/linky-pomoci.ts
@@ -0,0 +1,7 @@
+/**
+ * linky-pomoci controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::linky-pomoci.linky-pomoci');
---
+++ src/api/linky-pomoci/routes/linky-pomoci.ts
@@ -0,0 +1,7 @@
+/**
+ * linky-pomoci router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::linky-pomoci.linky-pomoci');
